5570371e6b56d5fe2f43b4fe670ee0cedd518396
coneofshame <doggycone18@gmail.com>
2017-02-01 10:20:02 -0800

    drawploy

--- myfile.py
+++ myfile.py
@@ -15,4 +15,18 @@ def DrawRectangle(Anyturtle, l, w):
         Anyturtle.rt(90)
 stan = turtle.Turtle()
 DrawRectangle(stan, 50, 100)
+
+
+def DrawPoly(Anyturtle, n):
+    Anyturtle.color('green')
+    theta = 360 / n
+    if n > 20:
+        for i in range(n):
+            Anyturtle.fd(10)
+            Anyturtle.lt(theta)
+    else:
+        for i in range(n):
+            Anyturtle.fd(50)
+            Anyturtle.lt(theta)
+DrawPoly(stan, 21)
 input()
