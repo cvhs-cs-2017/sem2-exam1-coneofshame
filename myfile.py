712a9e06fdef48df6fea94c35fb8a33bb1e954ea
coneofshame <doggycone18@gmail.com>
2017-02-01 10:10:13 -0800

    drawRectangle is done

--- myfile.py
+++ myfile.py
@@ -2,3 +2,17 @@ def add10(n):
     n = n + 10
     return n
 print(add10(10))
+
+
+
+import turtle
+def DrawRectangle(Anyturtle, l, w):
+    Anyturtle.color('purple')
+    for i in range (2):
+        Anyturtle.fd(l)
+        Anyturtle.rt(90)
+        Anyturtle.fd(w)
+        Anyturtle.rt(90)
+stan = turtle.Turtle()
+DrawRectangle(stan, 50, 100)
+input()
