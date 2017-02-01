11e6ce0ed6ca6404a85860b65f7c8668df498559
coneofshame <doggycone18@gmail.com>
2017-02-01 11:46:48 -0800

    binarytonumber

--- ExtraCredit.optional.py
+++ ExtraCredit.optional.py
@@ -1,9 +1,21 @@
 """Write a program that will take a Binary number and return its Whole Number Value:
 
 example:  10011 would return (16+0+0+2+1 = 19)"""
-
-
-
+def binarytoNumber(binary):
+    num = 0
+    binary2 = ''
+    charCount = 0
+    for ch in str(binary):
+        binary2 = ch + binary2
+    for ch in str(binary2):
+        if ch == '0':
+            power = 0
+        else:
+            power = (2 ** charCount)
+        num += power
+        charCount += 1
+    return num
+print(binarytoNumber(1001))
 
 
 
