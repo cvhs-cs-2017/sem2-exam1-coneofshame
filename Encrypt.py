60169e93fdc43618ebc6759d1e5efcf91f506fad
coneofshame <doggycone18@gmail.com>
2017-02-01 10:38:22 -0800

    my own encypt

--- Encrypt.py
+++ Encrypt.py
@@ -15,7 +15,31 @@ def NoVowels(plaintext):
             ciphertext += ch
     return ciphertext
 
-varible = NoVowels('Computer Science Makes the World go round but it doesn\'t make the world round itself!')
-print(varible)
+variable = NoVowels('Computer Science Makes the World go round but it doesn\'t make the world round itself!')
+print(variable)
 
 """Write an encryption code that you make up and run it for the variable NoVowels"""
+def scribble(plaintext):
+    ciphertext = ''
+    halflength = int(len(plaintext) / 2)
+    fronthalf = plaintext[:halflength]
+    backhalf = plaintext[halflength:]
+    fronthalf2 = ''
+    for ch in fronthalf:
+        fronthalf2 = ch + fronthalf2
+    charCount = 0
+    even = ''
+    odd = ''
+    for ch in fronthalf2:
+        if charCount % 2 is 0:
+            even += ch
+        else:
+            odd += ch
+        charCount += 1
+    fronthalf = even + odd
+    backhalf2 = ''
+    for ch in backhalf:
+        backhalf2 = ch + backhalf2
+    ciphertext = backhalf2 + fronthalf2
+    return ciphertext
+print(scribble(variable))
