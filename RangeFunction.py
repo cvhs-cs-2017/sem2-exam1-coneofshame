307a46c9bd09ce11c61f55cb236905a304a28b51
coneofshame <doggycone18@gmail.com>
2017-02-01 10:53:39 -0800

    count 40-1

--- RangeFunction.py
+++ RangeFunction.py
@@ -1,6 +1,8 @@
 """Use the range function to print the numbers from 40 to 1 (backwards)"""
-
-
+def count():
+    for i in range(40, 0, -1):
+        print(i)
+print(count())
 
 """Repeat the exercise but count by 5's"""
 
